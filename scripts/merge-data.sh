5184c6762ae272d17173d7bb4189bf133e6db483
erri120 <erri120@protonmail.com>
2024-04-03 16:13:22 +0200

    Update merger

--- scripts/merge-data.sh
+++ scripts/merge-data.sh
@@ -4,12 +4,14 @@ current_dir="$(dirname -- "$(readlink -f -- "$0";)";)"
 tmp_dir="$(dirname $current_dir)/tmp"
 output_dir="$(dirname $current_dir)/data"
 tags_file="$output_dir/tags.json"
-output_file="$output_dir/smapi-game-versions.json"
+output1_file="$output_dir/smapi-game-versions.json"
+output2_file="$output_dir/game-smapi-version.json"
 
 echo "current directory: $current_dir"
 echo "tmp directory: $tmp_dir"
 echo "tags file: $tags_file"
-echo "output file: $output_file"
+echo "output1 file: $output1_file"
+echo "output2 file: $output2_file"
 
 mkdir -p $current_dir
 mkdir -p $tmp_dir
@@ -36,7 +38,12 @@ while read tag; do
 done < <(echo "$tags")
 
 echo "calling jq"
-res=$(jq -s 'map({(.SMAPIVersion): {MinimumGameVersion: .MinimumGameVersion, MaximumGameVersion: .MaximumGameVersion}}) | add' $input_files)
+
+res1=$(jq -s 'map({(.SMAPIVersion): {MinimumGameVersion: .MinimumGameVersion, MaximumGameVersion: .MaximumGameVersion}}) | add' $input_files)
+
+res2=$(jq -s 'map(select(.MinimumGameVersion != null)) | group_by(.MinimumGameVersion) | map({(.[0].MinimumGameVersion): map(.SMAPIVersion)}) | add' $input_files)
+
 echo "done"
 
-echo "$res" > $output_file
+echo "$res1" > $output1_file
+echo "$res2" > $output2_file
